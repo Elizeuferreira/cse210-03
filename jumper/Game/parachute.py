d1270bde0a32c543926a7e3d94ae383f5006820d
AlexanderKarasik <97298711+AlexanderKarasik@users.noreply.github.com>
2022-02-10 22:05:55 +0300

    parachute.py
    My version of parachute

--- jumper/Game/parachute.py
+++ jumper/Game/parachute.py
@@ -1,43 +1,36 @@
-def showParachute(n):
+class Jumper:
+    def __init__(self):
+        
+        self.line0 = ("")
+        self.line1 = ("  ___")
+        self.line2 = (" /___\ ")
+        self.line3 = (" \   /")
+        self.line4 = ("  \ /")
+        self.line5 = ("   0")
+        self.line6 = ("  /|\ ")
+        self.line7 = ("  / \ ")
+        self.line8 = ("")
+        self.line9 = ("^^^^^^^" )
+        self.is_playing = True
 
-    parachute0 = "\
-      ___ \n \
-    /___\ \n \
-    \   / \n \
-     \ / \n \
-      0 \n \
-     /|\  \n \
-     / \ \n "
-
-    parachute1 = "\
-     /___\ \n \
-    \   / \n \
-     \ / \n \
-      0 \n \
-     /|\  \n \
-     / \ \n "
     
-    parachute2 = "\
-     \   / \n \
-     \ / \n \
-      0 \n \
-     /|\  \n \
-     / \ \n "
 
-    parachute3 = "\
-      \ / \n \
-      0 \n \
-     /|\  \n \
-     / \ \n "
+    def display_parachute(self, number_of_mistakes):
 
-    parachute4 = "\
-       x \n \
-     /|\  \n \
-     / \ \n "
+        lines = [self.line0,self.line1,self.line2,self.line3,self.line4,self.line5,self.line6,self.line7,self.line8,self.line9]
+      
+        for line in lines:
+            index = lines.index(line)
+            if index < (number_of_mistakes+1):
+                line = ""
+            print(line)
+        if number_of_mistakes >= 4:
+            print("Game over\n")
+            self.is_playing = False
 
-    dictParachute = {
-        "0" : parachute0,
-        "1": parachute1,
+jumper = Jumper()
+jumper.display_parachute(4)
+
         "2": parachute2,
         "3": parachute3,
         "4": parachute4,
