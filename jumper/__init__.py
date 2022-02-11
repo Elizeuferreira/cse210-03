650a058bbfe875c265400f2efdca80a48236d35c
AlexanderKarasik <97298711+AlexanderKarasik@users.noreply.github.com>
2022-02-11 22:23:49 +0300

    Update __init__.py
    Added parachute class along with the function to display it.

--- jumper/__init__.py
+++ jumper/__init__.py
@@ -14,11 +14,38 @@ class Jumper:
     
     pass
 
-class parachute:
+class Parachute:
     
     #Alexander
     
-    pass
+    def __init__(self):
+
+        self.line0 = ("")
+        self.line1 = ("  ___")
+        self.line2 = (" /___\ ")
+        self.line3 = (" \   /")
+        self.line4 = ("  \ /")
+        self.line5 = ("   0")
+        self.line6 = ("  /|\ ")
+        self.line7 = ("  / \ ")
+        self.line8 = ("")
+        self.line9 = ("^^^^^^^" )
+        self.is_playing = True
+
+
+
+    def display_parachute(self, number_of_mistakes):
+
+        lines = [self.line0,self.line1,self.line2,self.line3,self.line4,self.line5,self.line6,self.line7,self.line8,self.line9]
+        if self.is_playing != False:
+            for line in lines:
+                index = lines.index(line)
+                if index < (number_of_mistakes+1):
+                    line = ""
+                print(line)
+            if number_of_mistakes >= 4:
+                print("Game over\n")
+                self.is_playing = False
 
 class word:
     
