21ec786f74673655249228c3482902b7101cee05
Elizeu Ferreira <77559581+Elizeuferreira@users.noreply.github.com>
2022-02-12 14:19:23 -0400

    Update __init__.py

--- jumper/__init__.py
+++ jumper/__init__.py
@@ -1,4 +1,4 @@
-""" The game package contains specific classes for playing jumper.
+""" Here is pecific classes for playing jumper.
 """
 #Elizeu Silva
 #Shawn Yang
@@ -6,14 +6,7 @@ from game.jumper import Jumper
 from game.parachute import Parachute
 from game.word import Word
 
-# Elizeu Silva
-# Shawn Yang
-from game.jumper import Jumper
-from game.parachute import Parachute
-from game.word import Word
-
 class Director:
-
 
     """A person who directs the game
     Attributes:
@@ -96,11 +89,39 @@ class Jumper:
             print("Game Over!")
             return False
 
-class parachute:
+class Parachute:
     
     #Alexander
     
-    pass
+    def __init__(self):
+
+        self.line0 = ("")
+        self.line1 = ("  ___")
+        self.line2 = (" /___\ ")
+        self.line3 = (" \   /")
+        self.line4 = ("  \ /")
+        self.line5 = ("   0")
+        self.line6 = ("  /|\ ")
+        self.line7 = ("  / \ ")
+        self.line8 = ("")
+        self.line9 = ("^^^^^^^" )
+        self.is_playing = True
+
+
+
+    def display_parachute(self, number_of_mistakes):
+
+        lines = [self.line0,self.line1,self.line2,self.line3,self.line4,self.line5,self.line6,self.line7,self.line8,self.line9]
+        if self.is_playing != False:
+            for line in lines:
+                index = lines.index(line)
+                if index < (number_of_mistakes+1):
+                    line = ""
+                print(line)
+            if number_of_mistakes >= 4:
+                print("Game over\n")
+                self.is_playing = False
+
 
 class word:
     
