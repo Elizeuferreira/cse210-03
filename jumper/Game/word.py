2d957382239fbc9f5f51c5b8eb2dbc56efb7ada5
Elizeu Ferreira <77559581+Elizeuferreira@users.noreply.github.com>
2022-02-11 23:03:06 -0400

    update_word

---
+++ jumper/Game/word.py
@@ -0,0 +1,30 @@
+import random
+#Elizeu 
+class Word:
+
+    def __init__(self):
+        self._words = [
+            "green",
+            "brown",
+            "blues",
+            "apple",
+            "books",
+            "magic",
+            "house",
+            "horse",
+            "uncle",
+            "angle"
+        ]
+        self._random_word = ''
+        
+    def new_word(self):
+        self._word = random.choice(self._words)
+        return self._word    
+        
+
+    def hide_word(self):
+        letters = []
+        letters = ', '.join(letters)
+        letters = ''.join(str(letters).split(','))
+        return letters
+  
