fc7c9c1ea32f35bbafafb20042a1fa43accee3a1
AlexanderKarasik <97298711+AlexanderKarasik@users.noreply.github.com>
2022-02-14 22:41:14 +0300

    Added comments to word.py

--- jumper/Game/word.py
+++ jumper/Game/word.py
@@ -1,11 +1,17 @@
 import random
-from random import randint
 
-class Guesser:
+class Word:
+    """
+    The instrument to create random word
+    """
     def __init__(self):
-        self.state = True
-        self.errors = 0
-        self.words = [
+        """
+        Constructs a list of words.
+        
+        Args:
+        self (Word): An instance of Word
+        """
+        self._words = [
             "green",
             "brown",
             "blues",
@@ -15,51 +21,17 @@ class Guesser:
             "house",
             "horse",
             "uncle",
-            "angle"
+            "angle",
+            "house"
         ]
-        self.letter0 = '_'
-        self.letter1 = "_"
-        self.letter2 = "_"
-        self.letter3 = "_"
-        self.letter4 = "_"
-        self.letters = [self.letter0,self.letter1,self.letter2,self.letter3,self.letter4]
-
-    def random_word(self):
-        indexRandom = randint(0,9)
-        word = self.words[indexRandom]
-        return word
-
-    def get_letters(self, random_word, chosen_letter):
-        letters = list(random_word)
-        for letter in letters:
-            index = letters.index(letter)
-            if chosen_letter == letter:
-                letter = chosen_letter
-            else:
-                letters[index] = "_"
-        return letters
-        #print(letters[0], letters[1], letters[2], letters[3], letters[4])
-
-        #print(self.letter0,self.letter1,self.letter2,self.letter3,self.letter4)
-
-    def display_prompt(self, prompt):
-        chosen_letter = str(input(prompt))
-        return chosen_letter
-
     
-            
+    def random_word(self):
+        """
+        Choose and return random word.
+        
+        Args:
+        self: an instance of Word
+        """
+        randomWord = random.choice(self._words)
+        return randomWord
 
-guesser = Guesser()
-random_word = guesser.random_word()
-#chosen_letter = guesser.display_prompt("Choose a letter: ")
-letters_final = ["_","_","_","_","_"]
-while letters_final[0] == "_" or letters_final[1] == "_" or letters_final[2] == "_" or letters_final[3] == "_" or letters_final[4] == "_":
-    chosen_letter = guesser.display_prompt("\nChoose a letter: ")
-    letters = guesser.get_letters(random_word, chosen_letter)
-    for letter in letters:
-        if letter in letters != "_":
-            index = letters.index(letter)
-            letters_final[index] = letter
-    print(letters_final[0], letters_final[1], letters_final[2], letters_final[3], letters_final[4])
-#def start_game(self):
-#        while self.letter0 != "-":
