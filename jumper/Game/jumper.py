fc6dd3bb5252fdd9b41c23ea7d50ae1160ee5bb3
Elizeu Ferreira <77559581+Elizeuferreira@users.noreply.github.com>
2022-02-15 13:54:06 -0400

    Update jumper.py

--- jumper/Game/jumper.py
+++ jumper/Game/jumper.py
@@ -1,5 +1,26 @@
-class Jumper:
-    
+"""class Jumper:
     #Brenner
+    #Code that Will Construct the Jumper.
+    def init_jumper():
+        global head,arms,legs
+        head=("   O")
+        arms=("  /|\ ")
+        legs=("  / \ ")
+        print(head)
+        print(arms)
+        print(legs)
     
-    pass
+    #Code that will first check
+    def fail_check():
+        lives=-1
+        if  lives >=0:
+            return True
+        else:
+            head=("   X")
+            print(head)
+            print(arms)
+            print(legs)
+            print()
+            print("Game Over!")
+            return False
+"""
