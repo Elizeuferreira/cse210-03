2b0ab39642505f7776f7e7027b4b8a2f512622f0
Elizeu Ferreira <77559581+Elizeuferreira@users.noreply.github.com>
2022-02-19 18:05:11 -0400

    Update jumper.py

--- jumper/Game/jumper.py
+++ jumper/Game/jumper.py
@@ -1,20 +1,4 @@
 from word import Word
 
 class Jumper:
-    def __init__(self):
-       self._word = Word()
-       self._hidden_word = self._word.random_word()
-       self._guess_word = ""
-    
-
-    def compare_word(self, listWord, guess_letter):
-        
-        for i in range(len(self._hidden_word)):
-            if guess_letter == self._hidden_word[i]:
-                listWord[i] = guess_letter
-
-    def _show_word_revelado(self, listWord):
-        guessWord = "".join(listWord)
-        self._guess_word = guessWord
-        word = " ".join(listWord)
-        print(word)
+   pass
