efa7610a5ce31ef3844feed1d047a2bb290ef21b
Elizeu Ferreira <77559581+Elizeuferreira@users.noreply.github.com>
2022-02-19 16:59:51 -0400

    alexander_chanced

--- jumper/Game/director.py
+++ jumper/Game/director.py
@@ -1,7 +1,6 @@
-from jumper import Jumper
 from parachute import Parachute
 from word import Word
-
+from jumper import Jumper 
 
 class Director:
 
@@ -23,15 +22,12 @@ class Director:
         self._playing = True
         self._parachute = Parachute()
         self._word = Word()
-        self._jumper = Jumper()
         self._hidden_word = self._word.random_word()
         self._guess_word = ""
         self._guess_is_true = None
         self.tries = 5
-      
-
+        self._jumper = Jumper()
     
-
     def start_game(self):
         """Starts the game by running the main game loop
         
@@ -44,12 +40,13 @@ class Director:
             guess_letter = self._get_inputs()
             self._guess_is_true = guess_letter in self._hidden_word
             if self._guess_is_true:
-                self._compare_word(listWord, guess_letter)
-                self._show_word_revelado(listWord)
-                self._parachute.show_parachute(self.tries)
+                self._jumper._compare_word(listWord, guess_letter)
+                self._jumper._show_word_revelado(listWord)
+                self._parachute._show_parachute(self.tries)
                 if self._guess_word == self._hidden_word:
                     print("YOU WIN!!!!")
-                    self._playing = False
+                    self._playing = False            
+        
             else:
                 self.tries -= 1
                 print("You still have",self.tries, "tries" )
@@ -74,32 +71,10 @@ class Director:
                     print("       \_______/           ")
 
                     self._playing = False
-
-
-    def _compare_word(self, listWord, guess_letter):
-        """
-        Compares letters in hiden word with a letter that the user guessed and 
-        make updates to the word to show
-        """
-        
-        for i in range(len(self._hidden_word)):
-            if guess_letter == self._hidden_word[i]:
-                listWord[i] = guess_letter
-
-    def _show_word_revelado(self, listWord):
-        """
-        Makes updates to display the correct word and displays it
-        """
-        guessWord = "".join(listWord)
-        self._guess_word = guessWord
-        word = " ".join(listWord)
-        print(word)
         
     def _get_inputs(self):
-        """Prompts the user to choose letter and returns it.
-        """
         guess_letter = input("Guess a letter [a-z]: ")
         return guess_letter
 
-
-                    
+               
+ 
