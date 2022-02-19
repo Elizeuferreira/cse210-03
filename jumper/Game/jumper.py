1d6953f7433baf7fa0588631b06332e26b67e498
AlexanderKarasik <97298711+AlexanderKarasik@users.noreply.github.com>
2022-02-20 01:18:39 +0300

    Update jumper.py

--- jumper/Game/jumper.py
+++ jumper/Game/jumper.py
@@ -1,26 +1,20 @@
-"""class Jumper:
-    #Brenner
-    #Code that Will Construct the Jumper.
-    def init_jumper():
-        global head,arms,legs
-        head=("   O")
-        arms=("  /|\ ")
-        legs=("  / \ ")
-        print(head)
-        print(arms)
-        print(legs)
-    
-    #Code that will first check
-    def fail_check():
-        lives=-1
-        if  lives >=0:
-            return True
-        else:
-            head=("   X")
-            print(head)
-            print(arms)
-            print(legs)
-            print()
-            print("Game Over!")
-            return False
-"""
+class Jumper():
+​
+    def __init__(self):
+        self._guess_word = ""
+        
+​
+    def _compare_word(self, listWord, guess_letter, hidden_word):
+        """
+        Compares letters in hiden word with a letter that the user guessed and 
+        make updates to the word to show
+        """
+        
+        for i in range(len(hidden_word)):
+            if guess_letter == hidden_word[i]:
+                listWord[i] = guess_letter
+​
+        guessWord = "".join(listWord)
+        self._guess_word = guessWord
+        word = " ".join(listWord)
+        return(word)
