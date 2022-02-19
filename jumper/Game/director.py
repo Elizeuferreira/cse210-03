4466ef76f7ed9676f9606af351ff5cabf9b7ea0d
Elizeu Ferreira <77559581+Elizeuferreira@users.noreply.github.com>
2022-02-19 19:50:23 -0400

    Update director.py

--- jumper/Game/director.py
+++ jumper/Game/director.py
@@ -1,9 +1,10 @@
+
+        
 from parachute import Parachute
 from word import Word
 from jumper import Jumper
 
 class Director:
-
     """A person who directs the game
     Attributes:
         jumper (Jumper): the game's jumper
@@ -22,35 +23,43 @@ class Director:
         self._playing = True
         self._parachute = Parachute()
         self._word = Word()
+        self._jumper = Jumper()
         self._hidden_word = self._word.random_word()
         self._guess_word = ""
         self._guess_is_true = None
         self.tries = 5
-        self._jumper = Jumper()
-
     
-
     def start_game(self):
         """Starts the game by running the main game loop
         
         Args:
             self (Director): an instance of Director.
         """
+        print()
+        print("_ _ _ _ _")   
         self._parachute._show_parachute(self.tries)
         listWord = list(len(self._hidden_word) * "_")
-        while self._playing:            
+        hidden_word = self._hidden_word
+        while self._playing:  
+            print()
             guess_letter = self._get_inputs()
             self._guess_is_true = guess_letter in self._hidden_word
             if self._guess_is_true:
-                self._jumper._compare_word(listWord, guess_letter)
-                self._jumper._show_word_revelado(listWord)
+                print()
+                word = self._jumper._compare_word(listWord, guess_letter, hidden_word)
+                print(word)
                 self._parachute._show_parachute(self.tries)
-                if self._guess_word == self._hidden_word:
+                if (str(word)).replace(" ","") == self._hidden_word:
                     print("YOU WIN!!!!")
                     self._playing = False
             else:
                 self.tries -= 1
+                print()
+                word = self._jumper._compare_word(listWord, guess_letter, hidden_word)
+                print(word)
+                print()
                 print("You still have",self.tries, "tries" )
+                print()
                 self._parachute._show_parachute(self.tries)
                 if self.tries == 0:
                     print("GAME OVER!")
@@ -70,18 +79,13 @@ class Director:
                     print("   \_             _/       ")
                     print("     \_         _/         ")
                     print("       \_______/           ")
-
                     self._playing = False
-
         
     def _get_inputs(self):
         """Prompts the user to choose letter and returns it.
         """
         guess_letter = input("Guess a letter [a-z]: ")
         return guess_letter
-
-
-
 
                
  
